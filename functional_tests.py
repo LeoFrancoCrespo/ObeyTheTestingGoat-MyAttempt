41768055dd87644ab72aeaf24362a75037660ea4
Leo <leo.francocrespo@gmail.com>
2018-10-04 11:40:23 -0500

    Refactor home page view to use template

--- functional_tests.py
+++ functional_tests.py
@@ -18,8 +18,34 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
-        
+
+        # invited to enter a to-do item straight away 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        # tries to put a task into a text box
+        inputbox.send_keys('Buy peackock feathers')
+
+        # page updates after pressing enter, 
+        # lists that task in a to-do list table 
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1) 
+
+        table = self.browser.find_element_by_id('id_lsit_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
+        # The text box is still there inviting adding another item 
         self.fail('Finish the test!')
+
+
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -6,20 +6,6 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    # resolve is used to resolve what view function a URL maps to 
-    def test_root_url_resolves_to_home_page_view(self):
-        found=resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_home_page_returns_correct_html(self):
-        #arrange 
-        request = HttpRequest()
-        
-        # act
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        
-        # assert
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
