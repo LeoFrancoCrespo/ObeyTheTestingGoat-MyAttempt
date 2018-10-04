fa2ce65f09ad8457b5ce6b2d596dc1b47fe02f1a
Leo <leo.francocrespo@gmail.com>
2018-10-04 11:27:36 -0500

    Functional test now checks that we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time 
 import unittest 
 
 class NewVisitorTest(unittest.TestCase):
@@ -12,7 +14,11 @@ class NewVisitorTest(unittest.TestCase):
         # trying to get onto homepage
         self.browser.get('http://localhost:8000')
 
+        # notice the title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
         self.fail('Finish the test!')
 
 if __name__ == '__main__':
