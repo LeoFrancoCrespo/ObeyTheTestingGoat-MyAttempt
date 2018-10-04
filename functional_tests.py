6ed77ed9a3524874e879689db84c5648564ac7ae
Leo <leo.francocrespo@gmail.com>
2018-10-04 14:46:12 -0500

    updated FT tests for handling multiple inputs into list

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # trying to get onto homepage
         self.browser.get('http://localhost:8000')
@@ -27,24 +32,24 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # tries to put a task into a text box
-        inputbox.send_keys('Buy peackock feathers')
-
         # page updates after pressing enter, 
         # lists that task in a to-do list table 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1) 
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # The text box is still there inviting adding another item 
-        self.fail('Finish the test!')
+        # now will add more items to the list 
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        
+        self.fail('Finish the test')
 
 
 
