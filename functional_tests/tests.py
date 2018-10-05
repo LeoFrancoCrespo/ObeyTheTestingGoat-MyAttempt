8a6efddf5f15969fff7447f6a3fdfe8fde305857
Leo <leo.francocrespo@gmail.com>
2018-10-05 15:04:15 -0500

    Updated html documents, form has an action to direct now

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -24,7 +24,7 @@ class NewVisitorTest(LiveServerTestCase):
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
                     raise e
-                time.sleep(0.5)
+                time.sleep(1)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # trying to get onto homepage
