a08f586d161f0652775fc11be593c2584fef1f94
Leo <leo.francocrespo@gmail.com>
2018-10-05 12:11:28 -0500

    increased sleep on FT so input could be processed

--- functional_tests.py
+++ functional_tests.py
@@ -36,7 +36,7 @@ class NewVisitorTest(unittest.TestCase):
         # lists that task in a to-do list table 
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1) 
+        time.sleep(3) 
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
@@ -44,7 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         self.check_for_row_in_list_table('1: Buy peacock feathers')
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
