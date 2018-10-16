6c895f47e013b6ce2f5e70a3fd3cd045f3fb585a
Leo <leo.francocrespo@gmail.com>
2018-10-16 12:46:18 -0500

    add a fabfile for automated deploys

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -25,7 +25,6 @@ def _get_latest_source():
 def _update_virtualenv():
     if not exists('virtualenv/bin/pip'):  
         run(f'python3.6 -m venv virtualenv')
-    run('cat requirements.txt')
     run('./virtualenv/bin/pip install -r requirements.txt') 
 
 def _create_or_update_dotenv():
